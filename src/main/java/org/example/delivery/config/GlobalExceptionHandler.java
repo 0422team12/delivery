7b39cac50ca52a649d147a7fe4bed24d1defcb53
le951 <ihyeongjin043@gmail.com>
2025-04-24 12:17:28 +0900

    fix: response 규격 오류 수정

--- src/main/java/org/example/delivery/config/GlobalExceptionHandler.java
+++ src/main/java/org/example/delivery/config/GlobalExceptionHandler.java
@@ -1,8 +1,18 @@
 package org.example.delivery.config;
 
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.ErrorResponse;
+import org.springframework.web.ErrorResponseException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ErrorResponseException.class)
+    public ProblemDetail handleErrorResponse(ErrorResponseException e){
+        return e.getBody();
+    }
+
 }
