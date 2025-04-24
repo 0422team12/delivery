2af33fe2de96a0374e796dbd9f8e31b9e5ae2e8f
le951 <ihyeongjin043@gmail.com>
2025-04-24 12:45:29 +0900

    chore: ExampleException 주석 추가

--- src/main/java/org/example/delivery/test/exception/ExampleException.java
+++ src/main/java/org/example/delivery/test/exception/ExampleException.java
@@ -9,11 +9,11 @@ public class ExampleException extends ErrorResponseException {
 
 //    ErrorResponse 필드 목록 (ErrorResponseException 구현체 기준)
 //
-//      private final HttpStatusCode status;            :
-//      private final HttpHeaders headers;              :
-//      private final ProblemDetail body;               :
-//      private final String messageDetailCode;         :
-//      private final Object[] messageDetailArguments;  :
+//      private final HttpStatusCode status;            : HttpStatus
+//      private final HttpHeaders headers;              : header 별도로 설정 시 정의
+//      private final ProblemDetail body;               : response body. 아래 ProblemDetail 참고
+//      private final String messageDetailCode;         : i18n 사용 시 정의
+//      private final Object[] messageDetailArguments;  : String messageDetailCode 값 치환
 //
 //    ErrorResponse 로 구현하면 표준적이고, Spring의 지원을 받을 수 있음.
 //    초기 학습 곡선이 꽤나 가파르지만
@@ -22,28 +22,18 @@ public class ExampleException extends ErrorResponseException {
 //    @ExceptionHandler() 를 작성하지 않아도 throw 시 자동으로 response.
 //
 //
-//    ProblemDetail 필드 목록 (표준)
+//    ProblemDetail 필드 목록 (RFC 9457)
 //
-//      private URI type;                       :
-//      private String title;                   :
-//      private int status;                     :
-//      private String detail;                  :
-//      private URI instance;                   :
+//      private URI type;                       : 해당 문제를 설명하는 링크 (default=about:blank)
+//      private String title;                   : 사람이 인지하기 위한 문제의 제목 (간략한 설명, 이름, HTTPStatus, ...)
+//      private int status;                     : HttpStatus
+//      private String detail;                  : 문제의 세부 설명
+//      private URI instance;                   : 오류가 발생한 URI
 //      private Map<String, Object> properties; : Custom 용도
 //
 //    ProblemDetail에 정의된 값이, throw 시 HTTP Response의 Body로 전달됨.
 
 //    자세한 get, set 등 메서드는 해당 구현체 참고
-
-//    이 예시 생성자의 결과
-//        this.headers = new HttpHeaders();
-//        this.status = status;
-//        this.body = ProblemDetail.forStatus(status);
-//        this.messageDetailCode = this.initMessageDetailCode((String) null);
-//        this.messageDetailArguments = (Object[]) null;
-    public ExampleException(HttpStatusCode status) {
-        super(status);
-    }
 
 
 //    아래와 같이 생성자를 정의하고, Controller, Service 등에서
@@ -92,6 +82,16 @@ public class ExampleException extends ErrorResponseException {
         body.setDetail("ExampleException's default constructor");
     }
 
+
+//    이 예시 생성자의 내부 구현
+//        this.headers = new HttpHeaders();
+//        this.status = status;
+//        this.body = ProblemDetail.forStatus(status);
+//        this.messageDetailCode = this.initMessageDetailCode((String) null);
+//        this.messageDetailArguments = (Object[]) null;
+    public ExampleException(HttpStatusCode status) {
+        super(status);
+    }
 
 }
 
