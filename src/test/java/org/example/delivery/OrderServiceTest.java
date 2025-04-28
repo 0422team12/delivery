4800a078cce734dd148c6857ddac1a9e6cbae4d8
류형철 <fbgudcjf2@gmail.com>
2025-04-28 15:57:41 +0900

    test(order): 테스트코드 작성

--- src/test/java/org/example/delivery/OrderServiceTest.java
+++ src/test/java/org/example/delivery/OrderServiceTest.java
@@ -1,15 +1,78 @@
 package org.example.delivery;
 
+import static org.mockito.BDDMockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.example.delivery.domain.cart.dto.response.CartItemResponse;
+import org.example.delivery.domain.cart.entity.Cart;
+import org.example.delivery.domain.cart.repository.CartItemRepository;
+import org.example.delivery.domain.cart.repository.CartRepository;
+import org.example.delivery.domain.order.repository.OrderRepository;
 import org.example.delivery.domain.order.service.OrderService;
+import org.example.delivery.domain.user.entity.User;
+import org.example.delivery.domain.user.enums.UserRole;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.http.HttpStatus;
 
-@SpringBootTest
+import java.util.List;
+import java.util.Optional;
+
 @ExtendWith(MockitoExtension.class)
-public class OrderServiceTest {
+class OrderServiceTest {
+
+    @Mock
+    CartRepository cartRepository;
+    @Mock
+    CartItemRepository cartItemRepository;
+    @Mock
+    OrderRepository orderRepository;
 
     @InjectMocks
-    private OrderService orderService;
+    OrderService orderService;
+
+    User user1 = new User("형철@naver.com", "123456789!A", UserRole.USER);
+    User user2 = new User("형진@naver.com", "123456789!A", UserRole.OWNER);
+    User user3 = new User("정연@naver.com", "123456789!A", UserRole.OWNER);
+    User user4 = new User("진아@naver.com", "123456789!A", UserRole.USER);
+    User user5 = new User("형진@naver.com", "123456789!A", UserRole.USER);
+
+    String str1 = "서울 강남구";
+    String str2 = "대전 유성구";
+    String str3 = "부산 해운대구";
+
+    @Test
+    void 장바구니가_없으면_생성시_NOT_FOUND가_발생한다() {
+
+        //given
+        given(cartRepository.findByUserId(user1.getId()))
+                .willReturn(Optional.empty());
+
+        //when
+        ResponseStatusException ex = assertThrows(
+                ResponseStatusException.class,
+                () -> orderService.createOrder(user1.getId(), str1)
+        );
+        //then
+        assertEquals(HttpStatus.NOT_FOUND, ex.getStatusCode());
+        assertEquals("장바구니를 찾을 수 없습니다.", ex.getReason());
+    }
+
+
+//    @Test
+//    void 생성시_장바구니에_아무것도_담겨있지않다면_NOT_FOUND가_발생한다() {
+//        //given
+//
+//        Cart cart1 = cartRepository.save(Cart.createCart(user1, null, null));
+//        Cart cart2 = cartRepository.save(Cart.createCart(user2, null, null));
+//
+//        //when
+//
+
+    }
 }
